eb79b1334b467cccb96e5ed408a65a80ed25f14f
Noah Katz <noahkatz@chicagobooth.edu>
2023-05-02 20:36:46 +0000

    photo create!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   def index
     matching_photos = Photo.all
-    @list_of_photos = matching_photos.order({:created_at => :asc})
+    @list_of_photos = matching_photos.order({:created_at => :desc})
     render({ :template => "photo_templates/index.html.erb"})
   end
 
@@ -16,6 +16,34 @@ class PhotosController < ApplicationController
     else
       render({ :template => "photo_templates/show.html.erb"})
     end
+  end
+
+  def delete
+    url_photo = params.fetch("toast_id")
+    matching_photo = Photo.where({:id => url_photo})
+
+    the_photo = matching_photo.first #this can be .at(0)
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/delete.html.erb"})
+    redirect_to("/photos")
+  end
+
+  def create
+    #Parameters: {"input_image"=>"1", "input_caption"=>"2", "input_owner_id"=>"3"}
+
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    input_owner_id = params.fetch("input_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_image
+    a_new_photo.owner_id = input_owner_id
+    a_new_photo.save
+
+    redirect_to("/photos/" + a_new_photo.id.to_s)
+    #render({ :template => "photo_templates/create.html.erb"})
   end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   get("/", {:controller => "users", :action => "index"})
   get("/users", {:controller => "users", :action => "index"})
   get("/photos", {:controller => "photos", :action => "index"})
+  get("/insert_photo", {:controller => "photos", :action => "create"})
   ###### Dynamic Below
   get("/users/:path_username", {:controller => "users", :action => "show"})
   get("/photos/:path_id", {:controller => "photos", :action => "show"})
+  get("/delete_photo/:toast_id", {:controller => "photos", :action => "delete"})
+  
+  #get("/insert_photo/:input_id", {:controller => "photos", :action => "delete"})
 
 end
